7189b138a2ae100d7d245b733bcd82934ef65913
Publio Mello <publio.lima.mello@gmail.com>
2025-12-22 19:02:56 -0300

    Class Card criada

--- scripts/Card.js
+++ scripts/Card.js
@@ -1,61 +1,77 @@
 import { openModal, closeModal } from "./utils.js";
 
-// Ceating card elements
-const template = document.querySelector("#cards__template");
+export class Card {
+  constructor(name, link) {
+    this._name = name;
+    this._link = link;
+  }
 
-export function getCardElement(
-  name = "Lugar sem nome",
-  link = "./images/placeholder.jpg"
-) {
-  //clone the template
-  const cardElement = template.content.cloneNode(true);
+  _getTemplate() {
+    return document
+      .querySelector("#cards__template")
+      .content.querySelector(".card")
+      .cloneNode(true);
+  }
 
-  //find elements inside the clone
-  const cardImage = cardElement.querySelector(".card__image");
-  const cardTitle = cardElement.querySelector(".card__title");
+  _handleLikeButton() {
+    this._likeButton.classList.toggle("card__like-button_is-active");
+  }
 
-  //Fill up the data
-  cardImage.src = link;
-  cardImage.alt = name;
-  cardTitle.textContent = name;
+  _handleDeleteCard() {
+    this._element.remove();
+    this._element = null;
+  }
 
-  //like button
-  const cardLikeButton = cardElement.querySelector(".card__like-button");
+  _handleOpenImage() {
+    this._popupImage.src = this._link;
+    this._popupImage.alt = this._name;
+    this._popupCaption.textContent = this._name;
+    openModal(this._imagePopup);
+  }
 
-  cardLikeButton.addEventListener("click", function () {
-    cardLikeButton.classList.toggle("card__like-button_is-active");
-  });
+  _setEventListeners() {
+    this._likeButton.addEventListener("click", () => {
+      this._handleLikeButton();
+    });
 
-  //Remove button
-  const cardDeleteButton = cardElement.querySelector(".card__delete-button");
+    this._deleteButton.addEventListener("click", () => {
+      this._handleDeleteCard();
+    });
 
-  cardDeleteButton.addEventListener("click", function () {
-    const card = this.closest(".card");
-    card.remove();
-  });
+    this._image.addEventListener("click", () => {
+      this._handleOpenImage();
+    });
 
-  //open the Image
-  const openImageButton = cardElement.querySelector(".card__image");
-  const imagePopup = document.querySelector("#image-popup");
-  const imagePopupValue = imagePopup.querySelector(".popup__image");
-  const imagePopupCaption = imagePopup.querySelector(".popup__caption");
+    this._closePopupButton.addEventListener("click", () => {
+      closeModal(this._imagePopup);
+    });
+  }
 
-  openImageButton.addEventListener("click", function () {
-    imagePopupValue.src = this.src;
-    imagePopupCaption.textContent = this.alt;
-    openModal(imagePopup);
-  });
+  generateCard() {
+    this._element = this._getTemplate();
 
-  // close image
-  const closeImagePopup = imagePopup.querySelector(".popup__close");
-  closeImagePopup.addEventListener("click", function () {
-    closeModal(imagePopup);
-  });
+    this._image = this._element.querySelector(".card__image");
+    this._title = this._element.querySelector(".card__title");
+    this._likeButton = this._element.querySelector(".card__like-button");
+    this._deleteButton = this._element.querySelector(".card__delete-button");
 
-  return cardElement;
+    this._imagePopup = document.querySelector("#image-popup");
+    this._popupImage = this._imagePopup.querySelector(".popup__image");
+    this._popupCaption = this._imagePopup.querySelector(".popup__caption");
+    this._closePopupButton = this._imagePopup.querySelector(".popup__close");
+
+    this._image.src = this._link;
+    this._image.alt = this._name;
+    this._title.textContent = this._name;
+
+    this._setEventListeners();
+
+    return this._element;
+  }
 }
 
 export function renderCard(name, link, container) {
-  const cardElement = getCardElement(name, link);
+  const card = new Card(name, link);
+  const cardElement = card.generateCard();
   container.prepend(cardElement);
 }
