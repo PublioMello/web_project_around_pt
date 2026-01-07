435b949b4f9a105951f05fee574ba345045f53a3
Publio Mello <publio.lima.mello@gmail.com>
2026-01-07 20:42:27 -0300

    Criado a classe do FormValidator

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -1,21 +1,29 @@
-export function showInputError(inputElement, errorMessage, formElected) {
-  const errorElement = formElected.querySelector(
-    `.${inputElement.name}-input-error`
-  );
-  inputElement.classList.add("form__input_type_error");
-  errorElement.textContent = errorMessage;
-  errorElement.classList.add("form__input-error_active");
-}
+export default class FormValidator {
+  constructor(formElement) {
+    this._formElement = formElement;
+  }
 
-export function hideInputError(inputElement, formElected) {
-  const errorElement = formElected.querySelector(
-    `.${inputElement.name}-input-error`
-  );
-  inputElement.classList.remove("form__input_type_error");
-  errorElement.textContent = "";
-  errorElement.classList.remove("form__input-error_active");
-}
+  showInputError(inputElement, errorMessage) {
+    const errorElement = this._formElement.querySelector(
+      `.${inputElement.name}-input-error`
+    );
 
-export function hasInvalidInput(inputs) {
-  return Array.from(inputs).some((input) => !input.validity.valid);
+    inputElement.classList.add("form__input_type_error");
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add("form__input-error_active");
+  }
+
+  hideInputError(inputElement) {
+    const errorElement = this._formElement.querySelector(
+      `.${inputElement.name}-input-error`
+    );
+
+    inputElement.classList.remove("form__input_type_error");
+    errorElement.textContent = "";
+    errorElement.classList.remove("form__input-error_active");
+  }
+
+  hasInvalidInput(inputs) {
+    return Array.from(inputs).some((input) => !input.validity.valid);
+  }
 }
